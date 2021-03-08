032be406447ddc31a4c6a7661007aac3f4592edc
Komal Chauhan <76562738+ikomalchauhan@users.noreply.github.com>
2021-03-08 17:58:12 +0530

    version 1.0

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>TODO List</h1>
+      <form>
+        <input type="checkbox" value="second" />
+        <label>First Task</label>
+        <br />
+        <input type="checkbox" value="second" />
+        <label>Second Task</label>
+        <br />
+        <input type="checkbox" value="third" />
+        <label>Third Task</label>
+        <br />
+        <input type="checkbox" value="fourth" />
+        <label>Fourth Task</label>
+        <br />
+        <input type="checkbox" value="fifth" />
+        <label>Fifth Task</label>
+      </form>
     </div>
   );
 }
